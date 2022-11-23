daf58129016ab5aa5ed684e3f0b7aad7a89d94c2
rodormz <rodormz98@gmail.com>
2022-11-22 23:51:08 -0600

    Add comments to parser.

--- parser.py
+++ parser.py
@@ -15,18 +15,26 @@ tipo = "-1"
 varsTipo = []  # multiple vars with same type ex: int a,b,c,d;
 
 # current active func(to save vars in its table)
+# Directorio de funciones y tablas de variables
+funcsDir = []  # {'name': "funcName", 'type': "int/float/void/programType", 'kind': "program/func"}
+varsTables = {}  # {'tableName': {'varName': {'type': "int/float", 'kind': "local/global"}}}
+# funcs/vars pre-data to save
+name = []
+tipo = "-1"
+varsTipo = []  # multiple vars with same type ex: int a,b,c,d;
+
+# current active func(to save vars in its table)
 activeFuncTable = "none"  # global should be outside of any function
-activeScope = "global"
-pOperators = []
-pOperands = []
-pTypes = []
-pAssigns = []
-pAssignsTypes = []
-pCompOperands = []
-pCompOperators = []
-pCompTypes = []
-quads = []
-pilaDim = []
+activeScope = "global" #Variable para validar el scope actual
+pOperators = [] #pila operadores para expresiones
+pOperands = [] #pila operandos para expresiones
+pTypes = [] #pila tipos para expresiones
+pAssigns = [] #pila para manejar valores pendientes a asignacion
+pAssignsTypes = [] #pila para manejar los tipos de los operandos pendientes en pila assigns
+pCompOperands = [] #pila para operandos de expresiones comparativas
+pCompOperators = [] #pila para operadores de expresiones comparativas
+pCompTypes = [] #pila para tipos en expresiones comparativas
+quads = [] #lista de cuaddruplos
 quadCont = 1 #contador para temporales
 contTempLocal = 0
 contTempGlobal = 0
@@ -34,21 +42,18 @@ pJumps = []#pila saltos
 tempsCont = 0 # Limite inferior
 paramCounter = 0
 paramPointer = 0
-dim = 0 #Contador para obtener dimensiones de array.
+#pointers para asignar memorias virtuales
 globalMemory = MemoryPointer("global",1000, 1000, 2000, 3000, 4000, 4999)
 localMemory = MemoryPointer("local", 5000, 5000, 6000, 7000, 8000, 8999)
 tempMemoryGlobal = MemoryPointer("temporal global", 9000, 9000, 10000, 11000, 12000, 12999)
 tempMemoryLocal = MemoryPointer("temporal local", 13000, 13000, 14000, 15000, 16000, 17999)
 constantsMemory = MemoryPointer("constant", 18000, 18000, 19000, 20000, 21000, 21999)
-pointersMemoryGlobal = MemoryPointer("pointers", 22000, 22000, 23000, 24000, 25000, 25999)
-pointersMemoryLocal = MemoryPointer("pointers", 26000, 26000, 27000, 28000, 29000, 29999)
-globalTempsTable = {}
-localTempsTable = {}
-constTable = {}
-arrId = ""
-arrType = ""
-scopeKey = ""
-r = 1
+globalTempsTable = {}#tabla para guardar temporales globales
+localTempsTable = {}#tabla para guardar temporales locales
+constTable = {}#tabla de constantes
+#Cubo semantico para validación semántica de tipos
+#2 niveles para tipos de operandos y el tercero para el operador
+#cada casilla almacena el resultado de la operacion entre esos tipos
 semanticCube = {
     'int': {
         'float': {
@@ -163,6 +168,7 @@ def p_qpEnd(p):
     '''qpEnd : empty'''
     quads.append(["END", "", "", ""])
 
+#Particion de la gramática, para guardar id de programa
 def p_SAVEPROGID(p):
     '''SAVEPROGID : id'''
     global activeFuncTable
@@ -177,14 +183,19 @@ def p_SAVEPROGID(p):
     # INICIAMOS EL GO TO MAIN
     quads.append(["GOTO", "", ""])
 
+#Auxiliar de regla de declaracion de variables
 def p_MOREVARS(p):
     '''MOREVARS : VARS
                 | empty'''
 
-
+#Regla de declaracion de variables
 def p_VARS(p):
     '''VARS : TIPO VARSAUX semicolon MOREVARS'''
 
+# def p_VARSAUX(p):
+#     '''VARSAUX : id VARSCOMMA
+#                | id ARRAYDIMENSION ARRAYDIMENSION VARSCOMMA semicolon'''
+#Regla para seguir declarando variables o salir de la declaración de variables
 def p_VARSCOMMA(p):
     '''VARSCOMMA : comma VARSAUX
                  | empty'''
@@ -193,17 +204,17 @@ def p_VARSCOMMA(p):
     global activeFuncTable
     global varsTipo
 
-
+#Regla para declaración de funciones
 def p_FUNCS(p):
     '''FUNCS : function FUNCTIPO SAVEFUNCID leftParenthesis FUNCPARAM rightParenthesis leftBracket qpFuncsPN5 FUNCSVARS qpFuncsPN5Pt2 qpFuncsPN6 FUNCSESTATUTOS return FUNCEXP semicolon qpFuncsPN7 PNRIGHTBTACKETFUNC MOREFUNCS'''
 
-
+#Regla auxiliar para seguir declarando más funciones o salir de declaración de funciones
 # to acept more than one func
 def p_MOREFUNCS(p):
     '''MOREFUNCS : FUNCS
                  | empty'''
 
-
+#Particion de regla, para marcar cambio de scope y borrar tabla de variables
 def p_PNRIGHTBTACKETFUNC(p):
     '''PNRIGHTBTACKETFUNC : rightBracket'''
     global activeFuncTable
@@ -243,7 +254,7 @@ def p_PNRIGHTBTACKETFUNC(p):
     varsTables.pop(activeFuncTable)
     activeFuncTable = "global"
 
-
+#Particion de regla, para guardar id
 def p_SAVEFUNCID(p):
     '''SAVEFUNCID : id'''
     # Punto neuralgico 1 para las funciones donde se almacena el nombre de la funcion, y su tipo.
@@ -266,7 +277,7 @@ def p_SAVEFUNCID(p):
     else:
         print("Error duplicated func id")
 
-
+#Particion regla tipo, para guardar tipo de funcion
 def p_FUNCTIPO(p):
     '''FUNCTIPO : TIPO 
                 | void'''
@@ -274,26 +285,26 @@ def p_FUNCTIPO(p):
     if (p.__getitem__(1) == "void"):  # pre-save the func type
         tipo = "void"
 
-
+#Regla para parametros de declaración de función
 def p_FUNCPARAM(p):
     '''FUNCPARAM : PARAM PARAMSCOMMA'''
 
-
+#Regla auxiliar de funcparam, para agregar más parametros o salir de regla
 def p_PARAMSCOMMA(p):
     '''PARAMSCOMMA : comma FUNCPARAM 
                     | empty'''
 
-
+#Regla para declaración de variables dentro de una función
 def p_FUNCSVARS(p):
     '''FUNCSVARS : VARS FUNCSVARS
                  | empty'''
 
-
+#Regla para acotar los estatutos validos para una función
 def p_FUNCSESTATUTOS(p):
     '''FUNCSESTATUTOS : ESTATUTO FUNCSESTATUTOS
                      | empty'''
 
-
+#Regla para acotar las expresiones validas en una función
 def p_FUNCEXP(p):
     '''FUNCEXP : EXPR 
                | empty'''
@@ -304,22 +315,23 @@ def p_FUNCEXP(p):
     quads.append(["RETURN", "","",pOperands.pop()])
     pTypes.pop()
 
+#Regla para definir un bloque
 def p_BLOQUE(p):
     '''BLOQUE : leftBracket BLOQESTATUTO rightBracket'''
 
-
+#Regla para estatutos
 def p_BLOQESTATUTO(p):
     '''BLOQESTATUTO : ESTATUTO BLOQESTATUTO 
                     | empty'''
 
-
+#Regla para definir tipo de dato
 def p_TIPO(p):
     '''TIPO : intType
            | floatType'''
     global tipo
     tipo = p.__getitem__(1)  # pre-save the var type
 
-
+#Particion de regla funcparam, para guardar tipo y id de parametro
 def p_PARAM(p):
     '''PARAM : TIPO id'''
     global name
@@ -344,9 +356,11 @@ def p_PARAM(p):
     else:
         print("Error duplicated param var id")
 
+#Regla auxiliar de variables para guardar id
 def p_VARSAUX(p):
     '''VARSAUX : VARSAUXID ARRAYDIMENSION ARRAYDIMENSION qpArrPN7 VARSCOMMA'''
 
+#Regla para definir estructura del indexamiento de una dimensión
 def p_VARSAUXID(p):
     '''VARSAUXID : id'''
     global name
@@ -453,6 +467,7 @@ def p_intArrDim(p):
     limInf = varsTables[scopeKey][last_key]["arrDims"][dim-1]["LI"]
     r = (limSup - limInf + 1) * r
 
+#Regla estatuto
 def p_ESTATUTO(p):
     '''ESTATUTO : ASSIGN 
                 | PRINT
@@ -461,7 +476,8 @@ def p_ESTATUTO(p):
                 | CICLO'''
 
 
-def p_VARIABLE(p): #5+a[3]
+#Regla para definir estructura de una Variable dimensionada y simple
+def p_VARIABLE(p):
     '''VARIABLE : qpExpPN1
                 | qpArrCallPN1 qpArrCallPN2 VARIABLEIDM qpArrCallPN5
                 | qpArrCallPN1 qpArrCallPN2 VARIABLEIDM qpArrCallPN4 VARIABLEIDM qpArrCallPN5'''
@@ -556,7 +572,7 @@ def p_qpArrCallPN4(p):
     dim = dim + 1
     pilaDim.pop()
     pilaDim.append(dim)
-    
+
 
 def p_qpArrCallPN5(p):
    '''qpArrCallPN5 : empty'''
@@ -599,29 +615,36 @@ def p_qpArrCallPN5(p):
        pOperators.pop()
        quadCont+=1
 
+#Regla para definir dimensión de variable
 def p_VARIABLEIDM(p):
     '''VARIABLEIDM : leftSqBracket EXPR qpArrCallPN3 rightSqBracket'''
 
+#Regla para expresión aritmética
 def p_EXPR(p):
     '''EXPR : TERMINO MASOMENOST qpExpPN4'''
 
+#Regla auxiliar para permitir suma resta de terminos
 def p_MASOMENOST(p):
     '''MASOMENOST : qpExpPN3 TERMINO qpExpPN4 MASOMENOST
                   | empty'''
+
+#Regla para término
 def p_TERMINO(p):
     '''TERMINO : FACTOR PORENTREF qpExpPN5'''
 
+#Regla para permitir la multiplicación/division de factores
 def p_PORENTREF(p):
     '''PORENTREF : qpExpPN2 FACTOR qpExpPN5 PORENTREF
                  | empty'''
 
-
+#Regla para definirlos elementos validos de un factor
 def p_FACTOR(p):
     '''FACTOR : VARIABLE
               | leftParenthesis minusSign FACTOR qpExpPN8 rightParenthesis
               | NUMERO
               | leftParenthesis qpExpPN6 EXPR rightParenthesis qpExpPN7
               | LLAMADA'''
+
 
 def p_VARIABLEFACTOR(p):
     '''VARIABLEFACTOR : VARIABLE'''
@@ -634,6 +657,7 @@ def p_VARIABLEFACTOR(p):
         except:
             print("No existe la variable " + p[1])
 
+#Punto neuralgico 1 de expresiones
 def p_qpExpPN1(p):
     '''qpExpPN1 : id '''
     pOperands.append(p[1])
@@ -645,16 +669,19 @@ def p_qpExpPN1(p):
         except:
             print("No existe la variable " + p[1])
 
+#PN 3 de expresiones
 def p_qpExpPN2(p):
     '''qpExpPN2 : multiplicationSign
                 | divisionSign'''
     pOperators.append(p[1])
 
+#PN3 de expresiones
 def p_qpExpPN3(p):
     '''qpExpPN3 : plusSign
                 | minusSign'''
     pOperators.append(p[1])
 
+#PN4 de expresiones
 def p_qpExpPN4(p):
     '''qpExpPN4 : empty'''
     global quadCont
@@ -684,6 +711,7 @@ def p_qpExpPN4(p):
             else:
                 print("Type mismatch")
 
+#PN5 de expresiones
 def p_qpExpPN5(p):
     '''qpExpPN5 : empty'''
     global quadCont
@@ -714,20 +742,24 @@ def p_qpExpPN5(p):
             else:
                 print("Type mismatch")
 
+#PN6 de expresiones
 def p_qpExpPN6(p):
     '''qpExpPN6 : empty'''
     pOperators.append("(")
 
+#PN7 de expresiones
 def p_qpExpPN7(p):
     '''qpExpPN7 : empty'''
     pOperators.pop()
 
+#PN8 de expresiones
 def p_qpExpPN8(p):
     '''qpExpPN8 : empty'''
     negValue = pOperands.pop()
     negValue = "-" + str(negValue)
     pOperands.append(negValue)
 
+#PN1 de asignación
 def p_qpAssignPN1(p):
     '''qpAssignPN1 : empty'''
     global quadCont
@@ -745,7 +777,7 @@ def p_qpAssignPN1(p):
         else:
             print("Type mismatch")
 
-
+#PN1 de input
 def p_qpInputPN1(p):
     '''qpInputPN1 : empty'''
     # PENDIENTE VALIDAR TIPOS
@@ -754,18 +786,21 @@ def p_qpInputPN1(p):
     pAssignsTypes.pop()
     quads.append(["INPUT", "", "", result])
 
+#PN1 de print
 def p_qPrintPN1(p):
     '''qpPrintPN1 : empty'''
     global quadCont
     result = pOperands.pop()
     quads.append(["PRINT", "", "", result])
 
+#PN2 de print
 def p_qPrintPN2(p):
     '''qpPrintPN2 : cteString'''
     global quadCont
     result = p[1]
     quads.append(["PRINT", "", "", result])
 
+#PN1 de bool
 def p_qpBoolPN1(p):
     '''qpBoolPN1 : empty'''
     operand = pOperands.pop()
@@ -773,7 +808,7 @@ def p_qpBoolPN1(p):
     pCompOperands.append(operand)
     pCompTypes.append(type)
 
-
+#PN de bool
 def p_qpBoolPN2(p):
     '''qpBoolPN2 : empty'''
     global quadCont
@@ -803,6 +838,7 @@ def p_qpBoolPN2(p):
     else:
         print("Type mismatch")
 
+#PN5 de funciones
 def p_qpFuncsPN5(p):
     '''qpFuncsPN5 : empty'''
     global tempsCont
@@ -813,14 +849,17 @@ def p_qpFuncsPN5(p):
     contTempGlobal = quadCont
     quadCont = 1
 
+#PN5 de funciones parte 2
 def p_qpFuncsPN5Pt2(p):
     '''qpFuncsPN5Pt2 : empty'''
     funcsDir[len(funcsDir)-1]["varsSize"] = len(varsTables[activeFuncTable])
 
+#PN6 de funciones
 def p_qpFuncsPN6(p):
     '''qpFuncsPN6 : empty'''
     funcsDir[len(funcsDir)-1]["startFunc"] = len(quads)+1
 
+#PN7 de funciones
 def p_qpFuncsPN7(p):
     '''qpFuncsPN7 : empty'''
     global tempsCont
@@ -830,10 +869,11 @@ def p_qpFuncsPN7(p):
     quads.append(["ENDFUNC", "", "", ""])
     quadCont = contTempGlobal
 
+#Regla de expresiones comparativas
 def p_EXPCOMPARATIVA(p):
     '''EXPCOMPARATIVA : EXPR qpBoolPN1 COMPARISONOP EXPR qpBoolPN2'''
 
-
+# Regla de operadores comparativos
 def p_COMPARISONOP(p):
     '''COMPARISONOP : greaterThan
                     | lessThan
@@ -842,6 +882,7 @@ def p_COMPARISONOP(p):
 
     pCompOperators.append(p[1])
 
+#PN1 de estatutos condicionales
 def p_COND(p):
     '''COND : if leftParenthesis EXPCOMPARATIVA qpCondPN1 rightParenthesis BLOQUE qpCondPN2
             | if leftParenthesis EXPCOMPARATIVA qpCondPN1 rightParenthesis BLOQUE qpCondPN3 else BLOQUE qpCondPN2'''
@@ -859,13 +900,15 @@ def p_qpCondPN1(p):
     else:
         print("If statement, type mismatch")
 
-
+#PN2 de Condiciones
 #llenar cuadruplo pendiente
 def p_qpCondPN2(p):
     '''qpCondPN2 : empty'''
     #llenar el goto que se encuentre en la posicion guardada en pila saltos, con dir. de cuadruplo siguiente
     quads[pJumps.pop()].append(len(quads))
 
+
+#PN3 de estatutos condicionales
 #GOTO si o si a fin de else
 def p_qpCondPN3(p):
     '''qpCondPN3 : empty'''
@@ -876,9 +919,11 @@ def p_qpCondPN3(p):
     #agregarlo a pila saltos como pendiente (es el ultimo cuadruplo actual de la pila)
     pJumps.append(len(quads) - 1)
 
+#PN1 de ciclos
 def p_qpCicloPN1(p):
     '''qpCicloPN1 : empty'''
 
+#PN2 de ciclos
 #llenar cuadruplo pendiente
 def p_qpCicloPN2(p):
     '''qpCicloPN2 : empty'''
@@ -889,6 +934,7 @@ def p_qpCicloPN2(p):
     else:
         print("While statement, type mismatch")
 
+#PN3 de ciclos
 #GOTO si o si a fin de else
 def p_qpCicloPN3(p):
     '''qpCicloPN3 : empty'''
@@ -896,6 +942,7 @@ def p_qpCicloPN3(p):
     quads.append(["GOTO","","",returnSt])
     quads[pJumps.pop()].append(len(quads))
 
+#PN2 de llamada
 def p_qpLlamadaPN2(p):
     '''qpLlamadaPN2 : empty'''
     global paramCounter
@@ -906,6 +953,7 @@ def p_qpLlamadaPN2(p):
     calledFuncId = pOperands.pop()#save it to generate the quad and, to use it in PN3
     quads.append(["ERA",calledFuncId,"",""])#func id saved in pOperands at PN1
 
+#PN3 de llamada
 def p_qpLlamadaPN3(p):
     '''qpLlamadaPN3 : empty'''
     global paramCounter
@@ -925,11 +973,13 @@ def p_qpLlamadaPN3(p):
     except:
         print("Params error in func: ", calledFuncId)
 
+#PN4 de llamada
 def p_qpLlamadaPN4(p):
     '''qpLlamadaPN4 : empty'''
     global paramCounter
     paramCounter = paramCounter + 1
 
+#PN5 de llamada
 def p_qpLlamadaPN5(p):
     '''qpLlamadaPN5 : empty'''
     global paramCounter
@@ -941,6 +991,7 @@ def p_qpLlamadaPN5(p):
     except:
         print("Params error in func: ", calledFuncId)
 
+#PN6 de llamada
 def p_qpLlamadaPN6(p):
     '''qpLlamadaPN6 : empty'''
     try:
@@ -950,7 +1001,7 @@ def p_qpLlamadaPN6(p):
     except:
         print("Params error in func: ", calledFuncId)
 
-
+#Regla para una llamada
 def p_LLAMADA(p):
     '''LLAMADA : LLAMADAID leftParenthesis qpLlamadaPN2 LLAMADAEXPR qpLlamadaPN5 rightParenthesis qpLlamadaPN6
                | LLAMADAID leftParenthesis qpLlamadaPN2 rightParenthesis qpLlamadaPN6'''
@@ -983,10 +1034,10 @@ def p_LLAMADA(p):
         #print("quads ", quads)
         #print("global temps table: ", globalTempsTable)
 
-    
-                
 
 
+
+#Particion de regla llamada, para guardar id de función
 def p_LLAMADAID(p):
     '''LLAMADAID : id'''
     #qpLlamadaPN1 - Validar si función está declarada.
@@ -1001,24 +1052,25 @@ def p_LLAMADAID(p):
     if flag == False:
         print("Función " , p[1], " no declarada")
 
+#Regla para llamadas a funciones de tipo void
 def p_LLAMADAVOID(p):
     '''LLAMADAVOID : LLAMADAID leftParenthesis qpLlamadaPN2 LLAMADAEXPR qpLlamadaPN5 rightParenthesis qpLlamadaPN6 semicolon
                | LLAMADAID leftParenthesis qpLlamadaPN2 rightParenthesis qpLlamadaPN6 semicolon'''
 
-
+#Particion de regla llamada funcion, para introducir puntos neuralgicos
 def p_LLAMADAEXPR(p):
     '''LLAMADAEXPR : EXPR qpLlamadaPN3 LLAMADAEXPRAUX'''
 
-
+#Regla auxiliar de llamadaexpr, para seguir agregando expresiones (nuevos argumentos para la funcion),  o salir
 def p_LLAMADAEXPRAUX(p):
     '''LLAMADAEXPRAUX : qpLlamadaPN4 comma LLAMADAEXPR
                       | empty'''
 
-
+#Regla para ciclo
 def p_CICLO(p):
     '''CICLO : while qpCicloPN1 leftParenthesis EXPCOMPARATIVA qpCicloPN2 rightParenthesis BLOQUE qpCicloPN3'''
 
-
+#Regla para aceptar constantes de tipo numero
 def p_NUMERO(p):
     '''NUMERO : int
               | float'''
@@ -1035,28 +1087,30 @@ def p_NUMERO(p):
         constantsMemory.updateVirtualAddressPointer()
         constTable[p[1]] = constantsMemory.getFloatAddress()
 
-
+#Regla para asignacion
 def p_ASSIGN(p):
     '''ASSIGN : VARIABLE equalSign EXPR semicolon qpAssignPN1
               | VARIABLE equalSign INPUT semicolon qpInputPN1'''
 
+#Regla para input
 def p_INPUT(p):
     '''INPUT : input'''
 
+#Regla para print
 def p_PRINT(p):
     '''PRINT : print leftParenthesis PRINTARGS rightParenthesis semicolon'''
 
-
+#Regla para manejar multiples argumentos del print
 def p_PRINTARGS(p):
     '''PRINTARGS : EXPR qpPrintPN1 EXPRARGSAUX
                  | qpPrintPN2 EXPRARGSAUX'''
 
-
+#Regla auxiliar de exprsargs, para salir o continuar imprimiendo argumentos
 def p_EXPRARGSAUX(p):
     '''EXPRARGSAUX : comma PRINTARGS
                    | empty'''
 
-
+#Regla vacia para la insersión de puntos neurálgicos
 def p_empty(p):
     'empty :'
     pass
